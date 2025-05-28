e54636ad835ed768d5f9e912208a3604693641ee
Val <Valeriya.V@protonmail.com>
2025-05-28 22:44:10 +0300

    Revert "month view ready (w/o good css)"
    This reverts commit 9aacc16bee1153fd1d6f9a186ed971bb19d6ab37.

---
+++ src/types/calendarTypes.ts
@@ -0,0 +1,15 @@
+export interface Date {
+    day: number
+}
+
+export interface Year {
+    year: number
+}
+
+export interface Weekday {
+    letter: string
+}
+
+export interface Month {
+    name: string
+}
