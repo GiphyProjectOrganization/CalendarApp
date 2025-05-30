6b7c5e819713c3e6e3b9cc9297c8be27ddaeb17d
Nedkov999 <cvetomirnedkov@abv.bg>
2025-05-31 00:48:46 +0300

    register stufs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
-import { connectDB } from "./db";
+import { client, connectDB } from "./db";
 import bcrypt from "bcrypt";
+import { Request, Response } from "express";
 
 const app = express();
 const PORT = 5000;
@@ -18,9 +19,17 @@ app.listen(PORT, () => {
   console.log(`Backend is running at http://localhost:${PORT}`);
 });
 
-app.post("/api/register", async (req, res) => {
-  const { username, firstName, lastName, password, phoneNumber, email } = req.body;
 
+// register
+app.post("/api/register", async (req: Request, res: Response): Promise<void> => {
+  const { username, firstName, lastName, password, phoneNumber, email } = req.body as {
+    username: string;
+    firstName: string;
+    lastName: string;
+    password: string;
+    phoneNumber: string;
+    email: string;
+  };
 
   if (
     !email?.trim() ||
@@ -30,33 +39,38 @@ app.post("/api/register", async (req, res) => {
     !firstName?.trim() ||
     !phoneNumber?.trim()
   ) {
-    res.status(500).json({ message: "Please enter valid credentials." });
-    return
+    res.status(400).json({ message: "Please enter valid credentials." });
+    return;
   }
 
   try {
-
+    //connect to mongoDB 
     const client = await connectDB();
-    const db = client.db("calendar");
-    const usersCollection = db.collection("users");
-    const existingUser = await usersCollection.findOne({ email });
+    const db = client.db("calendar"); // choose database 
+    const usersCollection = db.collection("users"); // choose collection 
 
+    //check if user is already exist 
+    const existingUser = await usersCollection.findOne({ email });
     if (existingUser) {
       res.status(409).json({ message: "Email already in use" });
-      return
+      return;
     }
 
+    // hash password
     const hashedPassword = await bcrypt.hash(password, 10);
+    //make the obj with hash password 
     const newUser = { username, email, firstName, lastName, phoneNumber, password: hashedPassword };
+    // insert object in database/collection
     await usersCollection.insertOne(newUser);
 
+    //return alert this way because backend don't have alert keyword
     res.status(201).json({ message: "User registered successfully" });
-    return
+    return;
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Server error" });
-    return
+    return;
   }
-})
+});
 
 
